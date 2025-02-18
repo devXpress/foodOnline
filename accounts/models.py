0c12c9985f449247906195b381f1dd584b5d37cc
Abasi Ekpenyon <ekpenyonabasi@gmail.com>
2025-02-18 20:05:15 +0100

    customer and vendor registration

--- accounts/models.py
+++ accounts/models.py
@@ -42,7 +42,7 @@ class User(AbstractBaseUser):
     CUSTOMER = 2
 
     ROLE_CHOICE = (
-        (VENDOR, 'Restaurant'),
+        (VENDOR, 'Vendor'),
         (CUSTOMER, 'Customer'),
     )
 
@@ -78,6 +78,13 @@ class User(AbstractBaseUser):
     
     def has_module_perms(self, app_label):
         return True
+    
+    def get_role(self):
+        if self.role == 1:
+            user_role = 'Vendor'
+        elif self.role == 2:
+            user_role = 'Customer'
+        return user_role
 
 
 class UserProfile(models.Model):
