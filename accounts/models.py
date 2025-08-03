8a99d86d7532b22897a12df660c0e7d4bb85e92b
devXpress <ekpenyonabasi@gmail.com>
2025-08-03 23:53:55 +0100

    customer and vendor reg

--- accounts/models.py
+++ accounts/models.py
@@ -44,8 +44,8 @@ class User(AbstractBaseUser):
     VENDOR=1
     CUSTOMER=2
     ROLE_CHOICE= (
-        (VENDOR,'vendor'),
-        (CUSTOMER,'customer')
+        (VENDOR,'Vendor'),
+        (CUSTOMER,'Customer')
         )
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
@@ -76,6 +76,13 @@ class User(AbstractBaseUser):
     
     def has_module_perms(self, app_label):
         return True
+    
+    def get_role(self):
+        if self.role == 1:
+            user_role = 'Vendor'
+        elif self.role == 2:
+            user_role = 'Customer'
+        return user_role
     
 
 class UserProfile(models.Model):
