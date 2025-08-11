5c3dca5d647b382043b21044be603eebe41e06c7
devXpress <ekpenyonabasi@gmail.com>
2025-08-11 12:52:51 +0100

    menu builder and category CRUD

--- accounts/validators.py
+++ accounts/validators.py
@@ -1,9 +1,17 @@
 from django.core.exceptions import ValidationError
 import os
 
+#def allowOnlyImages(value):
+    #ext = os.path.splitext(value.name)[1]
+    #print(ext)
+    #valid_extensions = ['png','jpg', 'jpeg']
+    #if not ext.lower() in valid_extensions:
+        #raise ValidationError('Unsupported file extensions. Allowed extensions: ' +str(valid_extensions))
 def allowOnlyImages(value):
-    ext = os.path.splitext(value.name)[1]
-    print(ext)
-    valid_extensions = ['png','jpg', 'jpeg']
-    if not ext.lower() in valid_extensions:
-        raise ValidationError('Unsupported file extensions. Allowed extensions: ' +str(valid_extensions))
+    ext = os.path.splitext(value.name)[1]  # ".jpg", ".PNG"
+    ext = ext[1:].lower()  # remove dot, make lowercase: "jpg"
+    valid_extensions = ['png', 'jpg', 'jpeg']
+    if ext not in valid_extensions:
+        raise ValidationError(
+            f'Unsupported file extension. Allowed extensions: {valid_extensions}'
+        )
--- vendor/urls.py
+++ vendor/urls.py
@@ -6,5 +6,12 @@ from accounts import views as AccountViews
 urlpatterns = [
         path('', AccountViews.vendorDashboard, name= 'vendor'),
         path('vprofile',views.vprofile, name='vprofile'),
+        path('menu_builder', views.menu_builder, name='menu_builder'),
+        path('menu_builder/category/<int:pk>', views.fooditems_by_category, name='fooditems_by_category'),
+
+        # category crud
+        path('menu_builder/category/add', views.add_category, name='add_category'),
+        path('menu_builder/category/edit/<int:pk>/', views.edit_category, name='edit_category'),
+        path('menu_builder/category/delete/<int:pk>/', views.delete_category, name='delete_category'),
         
 ]
